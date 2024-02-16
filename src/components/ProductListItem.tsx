d642134910fca14ea87a6eb3135d8ca530ca10ed
cZAlpha <nbklaus21@students.desu.edu>
2024-02-15 19:24:12 -0500

    Typescript pathing

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../constants/Colors';
-import products from '../../assets/data/products';
+import products from '@assets/data/products';
 import { Product } from '../types';
 
 export const defaultPizzaImage = 
