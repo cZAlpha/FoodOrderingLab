0d8f69d41b325f4450dc319a68bfe2f538174fbd
cZAlpha <nbklaus21@students.desu.edu>
2024-02-17 14:14:34 -0500

    removedmodal

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Platform } from 'react-native'
 
 const cart = () => {
   return (
     <View>
       <Text>cart</Text>
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
+    
   )
 }
 
