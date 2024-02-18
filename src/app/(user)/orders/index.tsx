0eab0c4e14e9a19568b069ff9d143c4d50e4a86d
cZAlpha <nbklaus21@students.desu.edu>
2024-02-17 20:13:54 -0500

    finallydoneohmgodthissucked

---
+++ src/app/(user)/orders/index.tsx
@@ -0,0 +1,10 @@
+import { Text, FlatList } from "react-native";
+import orders from "@assets/data/orders";
+import OrderListItem  from "@/components/OrderListItem";
+
+export default function OrdersScreen() {
+ return (
+    <FlatList data={orders} renderItem={ ({item}) => <OrderListItem order={item} /> } 
+    />
+ );
+}
