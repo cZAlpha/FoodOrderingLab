8500502032d7669955136b7feb051497c00f9695
cZAlpha <nbklaus21@students.desu.edu>
2024-02-22 13:43:24 -0500

    layoutofadmindone

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -7,37 +7,37 @@ export default function MenuStack() {
     return (
         <Stack>
             <Stack.Screen name="index" options={{ title: "Menu",  
-                headerLeft: () => ( // CUSTOM (MADE BY NOAH) BUTTON TO GO BACK TO INDEX ROUTE
+                headerLeft: () => ( // CUSTOM (MADE BY NOAH) BUTTON TO GO BACK TO INDEX (Home Page) ROUTE
                     <Link href="/" asChild>
                         <Pressable>
                             {({ pressed }) => (
                                 <FontAwesome
-                                    name="backward"
-                                    size={20}
-                                    color={Colors.light.tint}
-                                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                                    name="backward" // sets the orientation of the icon to backward to make it look like a back button
+                                    size={20} // sets the size of the icon
+                                    color={Colors.light.tint} // sets the color of the icon
+                                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }} // Sets the right-side margin as well as styling when it is pressed
                                 />
                             )}
                         </Pressable>
-                    </Link>
-                ), 
-                headerRight: () => ( // ADD BUTTON (NOT DONE)
-                    <Link href="/(admin)/menu/create" asChild> 
-                        <Pressable>
+                    </Link> 
+                ),  // END OF CUSTOM BACK BUTTON 
+                headerRight: () => ( // ADD BUTTON - Sends user to the route below upon clicking the button
+                    <Link href="/(admin)/menu/create" asChild>  
+                        <Pressable> 
                             {({ pressed }) => (
                                 <FontAwesome
-                                    name="plus"
-                                    size={20}
-                                    color={Colors.light.tint}
-                                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                                    name="plus" // Makes the icon a plus sign
+                                    size={20} // Defines the size of the icon
+                                    color={Colors.light.tint} // Makes it a blue color using imported library
+                                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }} // Sets the right-side margin as well as styling when it is pressed
                                 />
                             )}
                         </Pressable>
                     </Link>
-                ), 
-                headerTitleStyle: {
-                    fontSize: 22, // Adjust the font size as desired
-                    fontWeight: 'bold', // Adjust the font weight as desired
+                ), // END OF ADD BUTTON
+                headerTitleStyle: { // This handles the styling of the title
+                    fontSize: 22, // font size of title
+                    fontWeight: 'bold', // sets font weight of title to bold
                 }
                 }}/>
         </Stack>
