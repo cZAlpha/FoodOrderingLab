b5a92f3dab7773710f0d0d74320f632434e41357
essmith20 <71906020+essmith20@users.noreply.github.com>
2024-02-23 10:46:54 -0500

    Update _layout.tsx

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,5 +1,7 @@
+//Imports Stack data structure.//
 import { Stack } from 'expo-router';
 
+//Exports the default authentication settings//
 export default function AuthLayout() {
   return <Stack />;
 }
