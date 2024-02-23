c9c030d585843d68e3cada05f68b43bb66377882
essmith20 <71906020+essmith20@users.noreply.github.com>
2024-02-23 10:52:58 -0500

    Update sign_up.tsx

--- src/app/(auth)/sign_up.tsx
+++ src/app/(auth)/sign_up.tsx
@@ -1,14 +1,19 @@
+
+//Import the react framework and its features as well button components, colors, the link and stack features, and the awsome font.//
+
 import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
 import React, { useState } from 'react';
 import Button from '../../components/Button';
 import Colors from '../../constants/Colors';
 import { Link, Stack } from 'expo-router';
 
+//Method for setting up the sign-up functionalities.//
 const SignUpScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
 
+  //Return the design of the sign-up screen.//
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: 'Sign up' }} />
@@ -41,6 +46,7 @@ const SignUpScreen = () => {
   );
 };
 
+//Holder for the styling of the sign-up screen.
 const styles = StyleSheet.create({
   container: {
     padding: 20,
@@ -71,5 +77,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
 });
-
+//Exports sign-up screen.//
 export default SignUpScreen;
